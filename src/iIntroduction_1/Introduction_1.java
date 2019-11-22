09800b474bd693da85c927522a4bc511ba5687e6
sindhubkrishna <sindhubkjobmail@gmail.com>
2019-11-22 13:14:42 +0530

    Intro

---
+++ src/iIntroduction_1/Introduction_1.java
@@ -0,0 +1,26 @@
+package iIntroduction_1;
+/**
+ * 
+Java is a High level language as well as framework.
+It is a Application level programming language where as C is a system level programming language.
+
+Major features of JAVA are 
+	1.Platform Independent 
+		Program can be written on any OS and it's complied .class can be executed on any other OS 
+	2.Open source 
+	3.Architectural neutral
+		It has constant 64 bit architecture.
+	4.No explicit pointers 
+	5.Garbage collectors 
+	6.Secured
+	7.Object Oriented
+	8.Multi threaded Environment 
+	
+Baisc flow
+	.java ---> [compile] ---> .class (byte code) ---> [execute] ---> .obj (MP+OS)
+	
+
+ */
+public class Introduction_1 {
+
+}
